7fd003d1bcf4dd8bf6b92dac254017234fe7a0c6
Awil Ali <60300580+awilali@users.noreply.github.com>
2026-06-26 15:34:21 +0100

    Added oidc policies in main.tf
    I have previously manually created the OIDC identity provider, OIDC IAM Role and its policies. I have now removed that and these resources will be created using IaC.

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -16,13 +16,14 @@ resource "aws_instance" "ec2_public_1" {
   }
 }
 
+## EC2 requires a instance profile as it can't assume the IAM role directly. 
+## Container that allows an IAM role to be attached to an EC2 instance.
+## EC2 → Instance Profile → IAM Role → Policies
 
-# EC2 profile to access the ecr
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "ec2-profile"
   role = aws_iam_role.ec2_role.name
 }
-
 
 # EC2 Role
 resource "aws_iam_role" "ec2_role" {
@@ -33,26 +34,133 @@ resource "aws_iam_role" "ec2_role" {
     Statement = [{
       Effect = "Allow"
       Principal = {
-        Service = "ec2.amazonaws.com"
+        Service = "ec2.amazonaws.com" #This is the service that assumes this role (and the permissions/policies that come with it)
       }
       Action = "sts:AssumeRole"
     }]
   })
 }
 
-# IAM Role policy or permission for ec2 to access ecr
+## IAM Role policy Allows an EC2 instance to pull and read Docker images from Amazon ECR.
 resource "aws_iam_role_policy_attachment" "ecr" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-#ssm policy
+## Allows an EC2 instance to be managed by AWS Systems Manager (SSM).
 resource "aws_iam_role_policy_attachment" "ssm_core" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-# ECR Resource
+## Github identity provider resource
+## GitHub Actions → OIDC Provider → IAM Role → Policies
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+
+## OIDC role
+resource "aws_iam_role" "oidc_role" {
+  name = "oidc-role"
+
+  assume_role_policy = jsonencode({
+  Version = "2012-10-17"
+  Statement = [{
+    Effect = "Allow"
+    Principal = {
+      Federated = aws_iam_openid_connect_provider.github.arn
+    }
+    Action = "sts:AssumeRoleWithWebIdentity"
+    Condition = {
+      StringEquals = {
+        "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+      }
+      StringLike = {
+        "token.actions.githubusercontent.com:sub" = "repo:awilali/aws-cicd-container-pipeline:*"
+      }
+    }
+  }]
+})
+
+## OIDC policy attachment
+resource "aws_iam_role_policy_attachment" "ssm_ec2_attach" {
+  role       = aws_iam_role.oidc_role.name
+  policy_arn = aws_iam_policy.ssm_ec2_deploy.arn
+}
+
+## 
+resource "aws_iam_policy" "ssm_ec2_deploy" {
+  name = "ssm-ec2-deploy-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:SendCommand",
+          "ssm:GetCommandInvocation",
+          "ssm:ListCommandInvocations"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeInstances"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+## IAM OIDC POLICY to push to ecr
+resource "aws_iam_role_policy_attachment" "ecr_full_access" {
+  role       = aws_iam_role.oidc_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+}
+
+## IAM OIDC POLICIES 
+resource "aws_iam_role_policy_attachment" "ec2_full_access" {
+  role       = aws_iam_role.oidc_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+## IAM OIDC - What is GitHub Actions allowed to do in AWS
+resource "aws_iam_role_policy" "iam_terraform_access" {
+  name = "iam-terraform-access"
+  role = aws_iam_role.oidc_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "iam:CreateRole",
+          "iam:DeleteRole",
+          "iam:AttachRolePolicy",
+          "iam:PutRolePolicy",
+          "iam:PassRole"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+
+## ECR Resource
 resource "aws_ecr_repository" "app" {
   name         = "my-app"
   force_delete = true
@@ -65,8 +173,7 @@ resource "aws_ecr_repository" "app" {
   image_tag_mutability = "MUTABLE"
 }
 
-# ECR Life cycle policy - This prevents ECR from filling up with old images.
-
+## ECR Life cycle policy - This prevents ECR from filling up with old images.
 resource "aws_ecr_lifecycle_policy" "app" {
   repository = aws_ecr_repository.app.name
 
