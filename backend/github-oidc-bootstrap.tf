8aa301bdc3de4a452b068c23daaa5a6f518d9046
Awil Ali <awil.aydiid@gmail.com>
2026-06-27 17:36:31 +0100

    updated iam.tf

--- backend/github-oidc-bootstrap.tf
+++ backend/github-oidc-bootstrap.tf
@@ -36,7 +36,7 @@ resource "aws_iam_role" "github_actions_role" {
           StringLike = {
             "token.actions.githubusercontent.com:sub" = [
               "repo:awilali/aws-cicd-container-pipeline:ref:refs/heads/main",
-              "repo:awilali/aws-cicd-container-pipeline:pull_request"
+              "repo:awilali/aws-cicd-container-pipeline:pull_request:refs/pull/*"
             ]
           }
         }
@@ -78,16 +78,65 @@ resource "aws_iam_policy" "ssm_github_ec2_deploy_policy" {
   })
 }
 
-## IAM OIDC POLICY to push to ecr
-resource "aws_iam_role_policy_attachment" "ecr_full_access" {
+resource "aws_iam_role_policy_attachment" "ecr_push_attach" {
   role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+  policy_arn = aws_iam_policy.github_ecr_push.arn
 }
 
-## IAM OIDC POLICIES 
-resource "aws_iam_role_policy_attachment" "ec2_full_access" {
+resource "aws_iam_policy" "github_ecr_push" {
+  name = "github-ecr-push-my-app"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:PutImage",
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer"
+        ]
+        Resource = "arn:aws:ecr:us-east-2:123456789012:repository/my-app"
+      }
+    ]
+  })
+}
+
+##
+resource "aws_iam_role_policy_attachment" "ec2_deploy_attach" {
   role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  policy_arn = aws_iam_policy.github_ec2_deploy.arn
+}
+
+##
+resource "aws_iam_policy" "github_ec2_deploy" {
+  name = "github-ec2-deploy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeInstances",
+          "ec2:DescribeInstanceStatus",
+          "ec2:DescribeTags"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 ## IAM OIDC - What is GitHub Actions allowed to do in AWS
@@ -108,6 +157,39 @@ resource "aws_iam_role_policy" "iam_terraform_access" {
           "iam:PassRole"
         ]
         Resource = "*"
+      }
+    ]
+  })
+}
+
+## S3 bucket - policy below is attached to it
+resource "aws_iam_role_policy_attachment" "tf_github_s3" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = aws_iam_policy.terraform_state_s3.arn
+}
+
+## S3 bucket - custom policy
+resource "aws_iam_policy" "terraform_state_s3" {
+  name = "terraform-state-s3"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket"
+        ]
+        Resource = "arn:aws:s3:::devops-project-s3bucket-eleanor"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = "arn:aws:s3:::devops-project-s3bucket-eleanor/*"
       }
     ]
   })
--- terraform/modules/ec2/iam.tf
+++ terraform/modules/ec2/iam.tf
@@ -24,10 +24,43 @@ resource "aws_iam_role" "ec2_role" {
 }
 
 ## IAM Role policy Allows an EC2 instance to pull and read Docker images from Amazon ECR.
-resource "aws_iam_role_policy_attachment" "ecr_attach_ec2" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+# resource "aws_iam_role_policy_attachment" "ecr_attach_ec2" {
+#   role       = aws_iam_role.ec2_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+# }
+
+
+resource "aws_iam_role_policy" "ecr_inline_policy" {
+  name = "ec2-ecr-readonly-inline"
+  role = aws_iam_role.ec2_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      # Authenticate with ECR
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+        Resource = "*"
+      },
+
+      # Pull images from ECR
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
+
+
 
 ## Allows an EC2 instance to be managed by AWS Systems Manager (SSM).
 resource "aws_iam_role_policy_attachment" "ssm_attach_ec2" {
